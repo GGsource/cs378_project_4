0cbed93c7fb2dc2119d465b678c239b2fabf0856
ggonza55 <ggonza55@uic.edu>
2022-11-28 11:25:29 -0600

    Initial Upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# cs378_project_4
+
+Flutter Project involving exploration of 2 Flutter packages and demonstrating them in use.
---
+++ android/app/src/main/kotlin/com/example/cs378_project_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cs378_project_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
